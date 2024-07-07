4fd9e1bf9a7bbb33fd2c2242711ba283cb37fbd0
Ali Choumane <ali.choumane@bial-x.com>
2024-07-07 19:31:10 +0200

    update packages

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:i3350_project1/main.dart';
+import 'package:flutter_first_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
